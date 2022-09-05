bde21702c667389d1d7dff303a89308a414fbf37
Junehyok Cho <jcho0627@gmail.com>
2022-09-05 20:18:35 +0200

    Add bill-to and ship-to columns in main table in homepage and work on adding checkbox input column

--- src/components/orders/OrderForm.jsx
+++ src/components/orders/OrderForm.jsx
@@ -11,70 +11,52 @@ function OrderForm() {
 
   const columns = [
     { accessor: "id", label: "id" },
+    { accessor: "bill-to", label: "Bill-To" },
+    { accessor: "ship-to", label: "Ship-To" },
     { accessor: "model", label: "Model" },
     { accessor: "quantity", label: "Quantity" },
-    // { accessor: "price", label: "Price", format: value => (value ? "✔️" : "✖️") },
     { accessor: "price", label: "Price"},
+    { accessor: "customer_po", label: "Customer PO"},
     { accessor: "line_status", label: "Line Status" },
+    // { accessor: "price", label: "Price", format: value => (value ? "✔️" : "✖️") },
   ]
 
-  const renderTbody = (item) => {
-    if (item === "") {
+  const Table = ({ columns, orders }) => {
+    if (orders === "") {
       return <tbody></tbody>
     } else {
       return (
-        <tbody>
-          {orders.map(order => {
-            return (
-              <tr key={order.id}>
+        <div className="container">
+          <table>
+            <thead>
+              <tr>
                 {columns.map(column => {
-                  return <td key={column.accessor}>{order[column.accessor]}</td>
+                  return <th key={column.accessor}>{column.label}</th>
                 })}
               </tr>
-            )
-          })}
-        </tbody>
-      )
-    }
-  }
-
-  const Table = ({ columns, orders }) => {
-      return (
-        <table>
-          <thead>
-            <tr>
-              {columns.map(column => {
-                return <th key={column.accessor}>{column.label}</th>
-              })}
-            </tr>
-          </thead>
-          {renderTbody(orders)}
-          {/* <tbody>
+            </thead>
+            <tbody>
               {orders.map(order => {
                 return (
                   <tr key={order.id}>
+                    <td>
+                      <input type="checkbox" name="select" />
+                    </td>
                     {columns.map(column => {
                       return <td key={column.accessor}>{order[column.accessor]}</td>
                     })}
                   </tr>
                 )
               })}
-          </tbody> */}
-        </table>
+            </tbody>
+          </table>
+        </div>
       )
+    }
   }
 
   return (
-    <>
-      <div className="container">
-        <Table orders={orders} columns={columns} />
-      </div>
-      <div className="controls">
-        <Button>Upload</Button>
-        <Button>Export</Button>
-        <Button type="submit" >Save</Button>
-      </div>
-    </>
+    <Table orders={orders} columns={columns} />
   )
 }
 
