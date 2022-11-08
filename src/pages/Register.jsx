aa12dbeb0a9d05eafd738e3dd27fc9c7d22c7833
Junehyok Cho <jcho0627@gmail.com>
2022-11-08 20:53:39 +0100

    add initial input field in Register page // require check on being able to add new orders even while not logged in

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,6 +1,27 @@
 function Register() {
+
+  const registerSubmit = (e) => {
+    e.preventDefault()
+    console.log("register request has been made")
+    console.log(e.target.reguname.value)
+    console.log(e.target.regpass.value)
+  }
+
   return (
-    <div>Register</div>
+    <form onSubmit={registerSubmit}>
+      <div className="input-container">
+        {/* {renderErrorMessage("nouser")} */}
+        <label>Username </label>
+        <input type="text" name="reguname" required />
+        {/* {renderErrorMessage("uname")} */}
+      </div>
+      <div className="input-container">
+        <label>Password </label>
+        <input type="password" name="regpass" required />
+        {/* {renderErrorMessage("pass")} */}
+      </div>
+      <button type="submit">Login</button>
+    </form>
   )
 }
 
