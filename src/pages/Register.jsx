b0d255088a75df4f47e666314a27ff2bef5011f4
Junehyok Cho <jcho0627@gmail.com>
2023-07-12 22:04:17 +0200

    install firebase and add initial setup / config in Register.jsx

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,3 +1,22 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDmTxmVLPLn9MTJkl0j4Q2YpZCsKFJgEF0",
+  authDomain: "orderflow-7aaed.firebaseapp.com",
+  projectId: "orderflow-7aaed",
+  storageBucket: "orderflow-7aaed.appspot.com",
+  messagingSenderId: "730677271573",
+  appId: "1:730677271573:web:463a99a8d06e936f40c21c",
+  measurementId: "G-BMTFE4TKDH"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Initialize Firebase Authentication and get a reference to the service
+const auth = getAuth(app);
+
 function Register() {
   const registerSubmit = (e) => {
     e.preventDefault();
@@ -19,7 +38,7 @@ function Register() {
         <input type="password" name="regpass" required />
         {/* {renderErrorMessage("pass")} */}
       </div>
-      <button type="submit">Login</button>
+      <button type="submit">Sign Up</button>
     </form>
   );
 }
