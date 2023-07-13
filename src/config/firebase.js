b99b54c05ad6676d5b562bcf34d540335f8f6c40
jcho0627 <jcho0627@gmail.com>
2023-07-13 13:29:40 +0200

    move firebase config // add initial registerSubmit

---
+++ src/config/firebase.js
@@ -0,0 +1,20 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDmTxmVLPLn9MTJkl0j4Q2YpZCsKFJgEF0",
+  authDomain: "orderflow-7aaed.firebaseapp.com",
+  projectId: "orderflow-7aaed",
+  storageBucket: "orderflow-7aaed.appspot.com",
+  messagingSenderId: "730677271573",
+  appId: "1:730677271573:web:463a99a8d06e936f40c21c",
+  measurementId: "G-BMTFE4TKDH"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Initialize Firebase Authentication and get a reference to the service
+const auth = getAuth(app);
+
+export { auth }
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,28 +1,26 @@
-import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
-
-const firebaseConfig = {
-  apiKey: "AIzaSyDmTxmVLPLn9MTJkl0j4Q2YpZCsKFJgEF0",
-  authDomain: "orderflow-7aaed.firebaseapp.com",
-  projectId: "orderflow-7aaed",
-  storageBucket: "orderflow-7aaed.appspot.com",
-  messagingSenderId: "730677271573",
-  appId: "1:730677271573:web:463a99a8d06e936f40c21c",
-  measurementId: "G-BMTFE4TKDH"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-
-// Initialize Firebase Authentication and get a reference to the service
-const auth = getAuth(app);
+import { auth } from "../config/firebase";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { useNavigate } from "react-router-dom";
 
 function Register() {
   const registerSubmit = (e) => {
+    const navigate = useNavigate();
+
     e.preventDefault();
     console.log('register request has been made');
     console.log(e.target.reguname.value);
     console.log(e.target.regpass.value);
+
+    createUserWithEmailAndPassword(auth, e.target.reguname.value, e.target.regpass.value)
+      .then((userCredential) => {
+        // Signed In
+        const user = userCredential.user
+        navigate("/")
+      })
+      .catch((error) => {
+        const errorCode = error.code
+        const errorMessage = error.message
+      })
   };
 
   return (
