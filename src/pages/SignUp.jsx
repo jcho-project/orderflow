21a8af7ce642d36331f7fed6298c1d12dc33159a
Junehyok Cho <jcho0627@gmail.com>
2023-07-28 22:26:01 +0200

    cleanup usused dark mode styling on SignUp and SignIn page

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -52,57 +52,43 @@ function Register() {
   }
 
   return (
-    <section className="bg-gray-50 dark:bg-gray-900">
+    <section className="bg-gray-50">
       <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-          <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+          <div className="w-full bg-white rounded-lg shadow md:mt-0 sm:max-w-md xl:p-0">
               <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
-                  <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+                  <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl">
                       Create your account
                   </h1>
                   <form onSubmit={registerSubmit} className="space-y-4 md:space-y-6" action="#">
                       <div>
-                          <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
-                          <input type="email" name="email" id="email" value={email} onChange={onChange} className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com" required="" />
+                          <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900">Your email</label>
+                          <input type="email" name="email" id="email" value={email} onChange={onChange} className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" placeholder="name@company.com" required="" />
                       </div>
                       <div>
-                          <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
-                          <input type="password" name="password" id="password" placeholder="••••••••" value={password} onChange={onChange} className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required="" />
+                          <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900">Password</label>
+                          <input type="password" name="password" id="password" placeholder="••••••••" value={password} onChange={onChange} className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" required="" />
                       </div>
                       <div>
-                          <label htmlFor="confirm-password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Confirm password</label>
-                          <input type="password" name="confirmPassword" id="confirmPassword" placeholder="••••••••" value={confirmPassword} onChange={onChange} className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required="" />
+                          <label htmlFor="confirm-password" className="block mb-2 text-sm font-medium text-gray-900">Confirm password</label>
+                          <input type="password" name="confirmPassword" id="confirmPassword" placeholder="••••••••" value={confirmPassword} onChange={onChange} className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" required="" />
                       </div>
                       <div className="flex items-start">
                           <div className="flex items-center h-5">
-                            <input id="terms" aria-describedby="terms" type="checkbox" className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800" required="" />
+                            <input id="terms" aria-describedby="terms" type="checkbox" className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300" required="" />
                           </div>
                           <div className="ml-3 text-sm">
-                            <label htmlFor="terms" className="font-light text-gray-500 dark:text-gray-300">I accept the <a className="font-medium text-primary-600 hover:underline dark:text-primary-500" href="#">Terms and Conditions</a></label>
+                            <label htmlFor="terms" className="font-light text-gray-500 dark:text-gray-300">I accept the <a className="font-medium text-primary-600 hover:underline" href="#">Terms and Conditions</a></label>
                           </div>
                       </div>
-                      <button type="submit" className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Create an account</button>
-                      <p className="text-sm font-light text-gray-500 dark:text-gray-400">
-                          Already have an account? <a href="/signin" className="font-medium text-primary-600 hover:underline dark:text-primary-500">Login here</a>
+                      <button type="submit" className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">Create an account</button>
+                      <p className="text-sm font-light text-gray-500">
+                          Already have an account? <a href="/signin" className="font-medium text-primary-600 hover:underline">Login here</a>
                       </p>
                   </form>
               </div>
           </div>
       </div>
     </section>
-    // <form onSubmit={registerSubmit}>
-    //   <div className="input-container">
-    //     {/* {renderErrorMessage("nouser")} */}
-    //     <label>Email</label>
-    //     <input type="email" label="Email Address" value={email} onChange={onChange} name="email" required />
-    //     {/* {renderErrorMessage("uname")} */}
-    //   </div>
-    //   <div className="input-container">
-    //     <label>Password</label>
-    //     <input type="password" label="Create Password" value={password} onChange={onChange} name="password" required />
-    //     {/* {renderErrorMessage("pass")} */}
-    //   </div>
-    //   <button type="submit">Sign Up</button>
-    // </form>
   );
 }
 
