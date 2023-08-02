b983d9b18f73cbe95730ee7156656e6f5dc85ee3
Junehyok Cho <jcho0627@gmail.com>
2023-08-02 21:53:41 +0200

    add template for Profile display page

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -30,12 +30,31 @@ function Profile() {
   console.log(auth.currentUser)
 
   return (
-    <div className="flex flex-col items-center justify-center bg-white border border-gray-200 rounded-lg shadow">
-      <FaUserCircle size={50} />
-      <h5 className="mb-1 text-xl font-medium text-gray-900">Junehyok Cho, {displayName}</h5>
-      <p className="text-sm text-gray-500">{email}</p>
-      <button type="button" onClick={onLogout} className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">Log Out</button>
-    </div>
+    // <div className="flex flex-col w-2/4 items-center justify-center bg-white border border-gray-200 rounded-lg shadow">
+    //   <FaUserCircle size={50} />
+    //   <h5 className="mb-1 text-xl font-medium text-gray-900">{displayName}</h5>
+    //   <p className="text-sm text-gray-500">{email}</p>
+    //   <button type="button" onClick={onLogout} className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">Log Out</button>
+    // </div>
+
+    <section className="flex font-medium items-center justify-center h-screen">
+      <section className="w-64 mx-auto bg-[#20354b] rounded-2xl px-8 py-6 shadow-lg">
+          <div className="flex items-center justify-between">
+              <span className="text-gray-400 text-sm">2d ago</span>
+              <span className="text-emerald-400">
+                  <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                </svg>
+              </span>
+          </div>
+
+          <div className="mt-8 ">
+              <h2 className="text-white font-bold text-2xl tracking-wide">{displayName}</h2>
+          </div>
+          <p className="text-emerald-400 font-semibold mt-2.5" >
+              Active
+          </p>
+      </section>
+    </section>
   )
 }
 
