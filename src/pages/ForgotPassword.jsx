48ba9abb96972d53104495d2ed79f9c9cc1e3fe1
Junehyok Cho <jcho0627@gmail.com>
2023-08-07 22:04:01 +0200

    add initial forgot password functionality // require style update to ForgotPassword page

--- src/pages/ForgotPassword.jsx
+++ src/pages/ForgotPassword.jsx
@@ -1,6 +1,43 @@
+import { useState } from "react"
+import { Link } from "react-router-dom"
+import { sendPasswordResetEmail } from "firebase/auth"
+import { auth } from "../config/firebase"
+
 function ForgotPassword() {
+  const [email, setEmail] = useState("")
+  
+  const onChange = (e) => {
+    setEmail(e.target.value)
+  }
+
+  const onSubmit = async (e) => {
+    e.preventDefault()
+
+    try {
+      await sendPasswordResetEmail(auth, email)
+      console.log("Email was Sent!")
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
   return (
-    <div>ForgotPassword</div>
+    <div>
+      <header>
+        <p>Forgot Password</p>
+      </header>
+
+      <main>
+        <form onSubmit={onSubmit}>
+          <input type="email" name="email" id="email" value={email} onChange={onChange} />
+          <Link className="forgotPasswordLink" to="/singin">
+            Sign In
+          </Link>
+          <div>Send Reset Link</div>
+          <button>Reset</button>
+        </form>
+      </main>
+    </div>
   )
 }
 
