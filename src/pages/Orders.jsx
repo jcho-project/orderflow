f570b58b3de6562bf3c8d1bb14a0351bf49039f1
June Hyok Cho <jcho0627@gmail.com>
2023-08-31 21:58:19 +0200

    create Orders page and /orders route // add changes of new routes to App.js // cleanup of unused OrderContext functions // require check on moving getOrders to OrderContext to be passed down to OrderList and OrderItem // require check on pass down of orderList as it is not being recognized as an array and an error while map occurs

--- src/pages/Orders.jsx
+++ src/pages/Orders.jsx
@@ -1,27 +1,19 @@
 import { useEffect, useState } from 'react';
-import { useNavigate } from 'react-router-dom';
 
 import OrderList from '../components/orders/OrderList';
 import { auth } from "../config/firebase"
 import SignIn from './SignIn';
 
-
 function Home() {
   const [user, setUser] = useState(null)
-  const navigate = useNavigate();
 
   useEffect(() => {
     setUser(auth.currentUser)
   })
 
-  const handleOrderRedirect = () => {
-    navigate("/orders")
-  }
-
   return (
     <>
-      <h1>Home</h1>
-      <button onClick={handleOrderRedirect}>Orders</button>
+      {user ? <OrderList /> : <SignIn />}
     </>
   );
 }
--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -31,7 +31,7 @@ function Login() {
       );
 
       const user = userCredential.user;
-      navigate("/profile")
+      navigate("/orders")
     } catch(error) {
       const errorCode = error.code;
       const errorMessage = error.message;
