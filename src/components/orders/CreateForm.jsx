e427f9808b36628da39c2a340394e371e1671055
June Hyok Cho <jcho0627@gmail.com>
2023-09-10 22:01:57 +0200

    add @apply styles for btn-primary and btn-secondary // check whether separate button componenet is a better solution // require styling on OrderList

--- src/components/orders/CreateForm.jsx
+++ src/components/orders/CreateForm.jsx
@@ -46,55 +46,60 @@ function CreateForm() {
   };
 
   return (
-    <div className="w-1/2 bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 flex flex-col my-2">
-      <h2 className="uppercase text-grey-darker text-xl font-bold mb-2">Sales Order Entry</h2>
-      <form onSubmit={handleSubmit} className="flex flex-col">
-        <h4 className="uppercase font-bold">Bill To</h4>
-        <select className="w-1/2 appearance-none bg-grey-lighter border border-grey-lighter text-grey-darker rounded" name="bill-to" value={values['bill-to']} onChange={handleInputChange}>
-          <option value=""></option>
-          <option value="MSH">MSH</option>
-          <option value="Saturn">Saturn</option>
-          <option value="OTTO">OTTO</option>
-        </select>
+    <>
+      <div className="w-full bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 flex flex-col my-2">
+        <h2 className="uppercase text-grey-darker text-xl font-bold mb-2">Sales Order Entry</h2>
+        <form onSubmit={handleSubmit} className="flex flex-col">
+          <h4 className="uppercase font-bold">Bill To</h4>
+          <select className="w-1/2 appearance-none bg-grey-lighter border border-grey-lighter text-grey-darker rounded my-2" name="bill-to" value={values['bill-to']} onChange={handleInputChange}>
+            <option value=""></option>
+            <option value="MSH">MSH</option>
+            <option value="Saturn">Saturn</option>
+            <option value="OTTO">OTTO</option>
+          </select>
 
-        <h4>Ship To</h4>
-        <select className="w-1/2 appearance-none bg-grey-lighter border border-grey-lighter text-grey-darker rounded" name="ship-to" value={values['ship-to']} onChange={handleInputChange}>
-          <option value=""></option>
-          <option value="New York">New York</option>
-          <option value="Boston">Boston</option>
-          <option value="Los Angeles">Los Angeles</option>
-        </select>
+          <h4 className="uppercase font-bold">Ship To</h4>
+          <select className="w-1/2 appearance-none bg-grey-lighter border border-grey-lighter text-grey-darker rounded my-2" name="ship-to" value={values['ship-to']} onChange={handleInputChange}>
+            <option value=""></option>
+            <option value="New York">New York</option>
+            <option value="Boston">Boston</option>
+            <option value="Los Angeles">Los Angeles</option>
+          </select>
 
-        <h4>Model</h4>
-        <input className="w-1/2 appearance-none bg-grey-lighter border border-grey-lighter text-grey-darker rounded" type="text" value={values['model']} name="model" onChange={handleInputChange} />
+          <h4 className="uppercase font-bold">Model</h4>
+          <input className="w-1/2 appearance-none bg-grey-lighter border border-grey-lighter text-grey-darker rounded my-2" type="text" value={values['model']} name="model" onChange={handleInputChange} />
 
-        <h4>Qty</h4>
-        <input
-          className="w-1/2 appearance-none bg-grey-lighter border border-grey-lighter text-grey-darker rounded"
-          type="number"
-          value={values['quantity']}
-          name="quantity"
-          onChange={handleInputChange}
-        />
+          <h4 className="uppercase font-bold">Qty</h4>
+          <input
+            className="w-1/2 appearance-none bg-grey-lighter border border-grey-lighter text-grey-darker rounded my-2"
+            type="number"
+            value={values['quantity']}
+            name="quantity"
+            onChange={handleInputChange}
+          />
 
-        <h4>Price</h4>
-        <input className="w-1/2 appearance-none bg-grey-lighter border border-grey-lighter text-grey-darker rounded" type="number" value={values['price']} name="price" onChange={handleInputChange} />
+          <h4 className="uppercase font-bold">Price</h4>
+          <input className="w-1/2 appearance-none bg-grey-lighter border border-grey-lighter text-grey-darker rounded my-2" type="number" value={values['price']} name="price" onChange={handleInputChange} />
 
-        <h4>Customer PO</h4>
-        <input
-          className="w-1/2 appearance-none bg-grey-lighter border border-grey-lighter text-grey-darker rounded"
-          type="text"
-          value={values['customer_po']}
-          name="customer_po"
-          onChange={handleInputChange}
-        />
+          <h4 className="uppercase font-bold">Customer PO</h4>
+          <input
+            className="w-1/2 appearance-none bg-grey-lighter border border-grey-lighter text-grey-darker rounded my-2"
+            type="text"
+            value={values['customer_po']}
+            name="customer_po"
+            onChange={handleInputChange}
+          />
 
-        <button className="w-1/2" type="submit">Submit</button>
-        <button className="w-1/2" type="button" onClick={() => navigate('/orders')}>
-          Close
-        </button>
-      </form>
-    </div>
+          <button className="w-1/2 btn-primary" type="submit">Submit</button>
+          <button className="w-1/2 btn-secondary" type="button" onClick={() => navigate('/orders')}>
+            Close
+          </button>
+        </form>
+        <div className="flex flex-col">
+          <h2>Test</h2>
+        </div>
+      </div>
+    </>
   );
 }
 
