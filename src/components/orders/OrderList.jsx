879c3d1a619d49bffb4d78d5d967fab613a7ceb9
June Hyok Cho <jcho0627@gmail.com>
2023-09-24 22:42:08 +0200

    fix editOrder and deleteOrder button in OrderItem // remove previous orderList table // remove PageTitle page as it is no longer needed // require check on addOrder button in navbar to be replaced by button in OrderList

--- src/components/orders/OrderList.jsx
+++ src/components/orders/OrderList.jsx
@@ -2,7 +2,6 @@ import { useContext, useEffect } from 'react';
 
 import OrderContext from '../../context/OrderContext';
 import OrderItem from './OrderItem';
-import PageTitle from './PageTitle';
 
 function OrderList() {
   const { orderList, getOrders } = useContext(OrderContext);
@@ -24,32 +23,6 @@ function OrderList() {
 
   return (
     <>
-      <div className="overflow-x-auto m-4">
-        <PageTitle />
-        <table className="w-full text-sm text-left text-gray-500">
-          <thead className="text-xs text-gray-700 uppercase bg-gray-100">
-            <tr>
-              {columns.map((column) => {
-                return (
-                  <th scope="col" className="px-6 py-4" key={column.accessor}>
-                    {column.label}
-                  </th>
-                );
-              })}
-              <th scope="col" className="px-6 py-4">
-                Edit
-              </th>
-              <th scope="col" className="px-6 py-4">
-                Delete
-              </th>
-            </tr>
-          </thead>
-          <tbody>
-            <OrderItem items={orderList} />
-          </tbody>
-        </table>
-      </div>
-
       {/* TEMPLATE */}
 
       {/* Breadcrumbs & Title */}
--- src/components/orders/PageTitle.jsx
+++
@@ -1,7 +0,0 @@
-function PageTitle() {
-  return (
-    <h1 className="text-2xl font-bold uppercase p-1 m-1 ">Sales Order Entry</h1>
-  )
-}
-
-export default PageTitle
