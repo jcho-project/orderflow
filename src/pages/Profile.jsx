9770263ab46958d251fbcb678018562c3be051f7
June Hyok Cho <jcho0627@gmail.com>
2023-10-04 22:48:12 +0200

    add form with inputs for displayName and email as well as edit button functionality // require Picked status data to be added in orderList

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -130,13 +130,42 @@ function Profile() {
                 <div className="mb-4 text-sm text-gray-500 dark:text-gray-400">
                     JPG, GIF or PNG. Max size of 800K
                 </div>
+                <div className="mt-8 ">
+                  <h2 className="text-white font-bold text-2xl tracking-wide">{name}</h2>
+                </div>
+                  <button
+                    className="px-3 py-2 text-sm font-medium text-center text-white rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+                    onClick={() => {
+                      changeDetails && onSubmit()
+                      setChangeDetails((prevState) => !prevState)
+                    }}>
+                      {changeDetails ? "DONE" : "EDIT"}
+                  </button>
+                  <form>
+                    <input
+                      type="text"
+                      id="displayName"
+                      className={!changeDetails ? "profileName" : "profileNameActive"}
+                      disabled={!changeDetails}
+                      value={displayName}
+                      onChange={onChange}
+                    />
+                    <input
+                      type="text"
+                      id="email"
+                      className={!changeDetails ? "profileEmail" : "profileEmailActive"}
+                      disabled={!changeDetails}
+                      value={email}
+                      onChange={onChange}
+                    />
+                  </form>
                 <div className="flex items-center space-x-4">
                   <button type="button" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">
                     <svg className="w-4 h-4 mr-2 -ml-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M5.5 13a3.5 3.5 0 01-.369-6.98 4 4 0 117.753-1.977A4.5 4.5 0 1113.5 13H11V9.413l1.293 1.293a1 1 0 001.414-1.414l-3-3a1 1 0 00-1.414 0l-3 3a1 1 0 001.414 1.414L9 9.414V13H5.5z"></path><path d="M9 13h2v5a1 1 0 11-2 0v-5z"></path></svg>
                     Upload picture
                   </button>
-                  <button type="button" className="py-2 px-3 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">
-                    Delete
+                  <button onClick={onLogout} type="button" className="py-2 px-3 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">
+                    Log Out
                   </button>
                 </div>
               </div>
