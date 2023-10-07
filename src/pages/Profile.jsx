d9216fd19366e1218182a3dfd1b49e4e9490240a
June Hyok Cho <jcho0627@gmail.com>
2023-10-07 22:39:13 +0200

    remove previous Profile structure and check edit and logout functionality

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -55,49 +55,6 @@ function Profile() {
 
   return (
     <>
-      {/* <section className="flex font-medium items-center justify-center h-screen">
-        <section className="w-64 mx-auto bg-[#20354b] rounded-2xl px-8 py-6 shadow-lg">
-            <div className="flex items-center justify-between">
-                <span className="text-gray-400 text-sm">2d ago</span>
-                <span className="text-emerald-400">
-                    <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                  </svg>
-                </span>
-            </div>
-
-            <div className="mt-8 ">
-                <h2 className="text-white font-bold text-2xl tracking-wide">{name}</h2>
-            </div>
-            <p className="text-emerald-400 font-semibold mt-2.5" >Active</p>
-            <p onClick={() => {
-              changeDetails && onSubmit()
-              setChangeDetails((prevState) => !prevState)
-            }}>{changeDetails ? "DONE" : "EDIT"}</p>
-
-            <form>
-              <input
-                type="text"
-                id="displayName"
-                className={!changeDetails ? "profileName" : "profileNameActive"}
-                disabled={!changeDetails}
-                value={displayName}
-                onChange={onChange}
-              />
-              <input
-                type="text"
-                id="email"
-                className={!changeDetails ? "profileEmail" : "profileEmailActive"}
-                disabled={!changeDetails}
-                value={email}
-                onChange={onChange}
-              />
-            </form>
-            <div onClick={onLogout}>
-              <PrimaryButton type={"button"}>Log Out</PrimaryButton>
-            </div>
-        </section>
-      </section> */}
-
       {/* Breadcrumbs & Title */}
       <div className="grid grid-cols-1 px-4 pt-6 xl:grid-cols-3 xl:gap-4 dark:bg-gray-900">
         <div className="mb-4 col-span-full xl:mb-2">
