999527e19ca8538b3c3e2e789c43aed1fb51ba40
June Hyok Cho <jcho0627@gmail.com>
2023-10-11 22:41:00 +0200

    add itemsInitialState to be used in initial state array as plugging that in directly into useState results in empty array // require check on updatedCheckedState functionality as it triggers render loops

--- src/components/orders/OrderItem.jsx
+++ src/components/orders/OrderItem.jsx
@@ -5,13 +5,20 @@ import OrderContext from '../../context/OrderContext';
 function OrderItem({ items }) {
   const { editOrder, deleteOrder } = useContext(OrderContext);
 
-  const [isChecked, setIsChecked] = useState(
-    new Array(items.length).fill(false)
-  )
+  const itemsInitialState = new Array(items.length).fill(false)
+
+  const [isChecked, setIsChecked] = useState(itemsInitialState)
 
   const handleOnChange = (index) => {
-    setIsChecked(!isChecked)
-    console.log(isChecked)
+    // setIsChecked(!isChecked)
+
+    const updatedCheckedState = isChecked.map((state, position) => {
+      position === index ? !state : state
+    })
+
+    setIsChecked(updatedCheckedState)
+
+    console.log("isChecked", isChecked)
     console.log(index)
   }
 
@@ -31,7 +38,7 @@ function OrderItem({ items }) {
       <tr className="hover:bg-gray-100 dark:hover:bg-gray-700" key={item.id}>
         <td className="w-4 p-4">
           <div className="flex items-center">
-            <input name="checkbox" onChange={handleOnChange} checked={isChecked} aria-describedby="checkbox-1" type="checkbox" className="w-4 h-4 border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:focus:ring-primary-600 dark:ring-offset-gray-800 dark:bg-gray-700 dark:border-gray-600" />
+            <input name="checkbox" onChange={handleOnChange(items.indexOf(item))} checked={isChecked[items.indexOf(item)]} aria-describedby="checkbox-1" type="checkbox" className="w-4 h-4 border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:focus:ring-primary-600 dark:ring-offset-gray-800 dark:bg-gray-700 dark:border-gray-600" />
           </div>
         </td>
         {columns.map((column) => {
